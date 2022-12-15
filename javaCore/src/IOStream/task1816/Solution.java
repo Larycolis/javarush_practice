7fc8216466eeec26e595f34e6e0351279422bbd0
Евгения <ebogdanova@nobilis.team>
2022-12-15 22:18:52 +0400

    Init

--- javaCore/src/IOStream/task1816/Solution.java
+++ javaCore/src/IOStream/task1816/Solution.java
@@ -1,4 +1,22 @@
 package IOStream.task1816;
 
+import java.io.FileReader;
+import java.io.IOException;
+
+/*
+Английские буквы
+*/
+
 public class Solution {
+    public static void main(String[] args) throws IOException {
+        FileReader reader = new FileReader(args[0]);
+        int count = 0;
+        while (reader.ready()) {
+            if (String.valueOf((char)(reader.read())).matches("[A-Za-z]")) {
+                count++;
+            }
+        }
+        System.out.println(count);
+        reader.close();
+    }
 }
